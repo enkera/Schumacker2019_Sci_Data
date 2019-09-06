c375c8602eb4a097672ed17a9676d6f9039f5534
Scott Schumacker <36344208+ScottSchumacker@users.noreply.github.com>
2019-09-06 00:16:53 -0700

    Update sleuth_script.R

--- R/sleuth_script.R
+++ R/sleuth_script.R
@@ -1,1 +1,340 @@
 # This script is for Sleuth
+# Scott Schumacker
+# Script for Scientific Data paper
+# Schumacker et al.
+
+# Download Bioconductor if you do not already have it installed
+## try http:// if https:// URLs are not supported
+source("https://bioconductor.org/biocLite.R")
+biocLite()
+
+# Download BiocUpgrade to update necessary Bioconductor packages if needed
+## try http:// if https:// URLs are not supported
+source("https://bioconductor.org/biocLite.R")
+biocLite("BiocUpgrade")     ## R version 2.15 or later
+
+# Download BiomaRt
+## try http:// if https:// URLs are not supported
+source("https://bioconductor.org/biocLite.R")
+biocLite("biomaRt")
+
+# Install cowplot and load for plots and visuals
+install.packages("cowplot")
+library(cowplot)
+
+install.packages("ggplot2")
+library(ggplot2)
+
+install.packages("ggrepel")
+library(ggrepel)
+
+# Download Sleuth
+source("http://bioconductor.org/biocLite.R")
+biocLite("rhdf5")
+install.packages("devtools")
+devtools::install_github("pachterlab/sleuth")
+
+# Load Sleuth into the project
+library(sleuth)
+
+# Modify the following line so the location of your kallisto results (each
+# in their own folder) 
+sample_id <- dir(file.path("/Users/ScottSchumacker/Desktop/SciDataPaper/MacRetData"))
+
+# Return the names on your samples to check that they all populate
+# in the console
+sample_id
+
+# Access each file path for the each sample. Again, paste the same
+# file path as before and also write the sample_id
+kal_dirs <- file.path("/Users/ScottSchumacker/Desktop/SciDataPaper/MacRetData",sample_id)
+
+# Return the file path to the results containing the kallisto outputs
+kal_dirs
+
+# Create a table (.txt) file and save
+# Load the table for the experimental design. Paste file path
+# of the experimental design table in quotations
+s2c <- read.table(file.path("/Users/ScottSchumacker/Desktop/SciDataPaper/MacRetTable.txt"),
+                  header = TRUE,
+                  stringsAsFactors = FALSE,
+                  sep = "\t")
+
+# Check the table to see if it appears in the console
+s2c
+
+# Add the directories as a final table column called path
+s2c <- dplyr::mutate(s2c, path = kal_dirs)
+
+# check the table to see if it appears in the console
+print(s2c)
+
+# Load biomaRt into the project
+library(biomaRt)
+
+# List the Possible Marts to view and pick one of your choice
+listMarts()
+
+# Choose which mart to use. Insert mart of choice betwwen quotation marks
+ensembl = useMart("ENSEMBL_MART_ENSEMBL")
+
+# List datasets within mart of choice to see which one you want/need
+listDatasets(ensembl)
+
+# Load genes of choice by defining which mart, dataset, and the host our 
+# data
+mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL",
+                         dataset = "hsapiens_gene_ensembl",
+                         host = "www.ensembl.org")
+
+# Get target transcripts from bioMart
+t2g <- biomaRt::getBM(
+  attributes = c("ensembl_transcript_id", "transcript_version",
+                 "ensembl_gene_id", "external_gene_name", "description",
+                 "transcript_biotype"), mart = mart)
+
+# Renaming the output from BioMart to make it easier to work
+# with in sleuth
+t2g <- dplyr::mutate(t2g, target_id = paste(ensembl_transcript_id, transcript_version, sep = "."))
+t2g <- dplyr::select(t2g, target_id, ens_gene = ensembl_gene_id, Gene_Name = external_gene_name, description, transcript_biotype)
+
+# Check your listing of transcripts to see if they populate in 
+# console. Not all transcripts will populate
+head(t2g)
+
+# Create the "Sleuth Object", a mathmatical
+# data structure that contains our results
+so <- sleuth_prep(s2c,
+                  target_mapping = t2g,
+                  extra_bootstrap_summary = TRUE,
+                  read_bootstrap_tpm = TRUE)
+
+# Check to see if target mapping worked with data and bioMArt was
+# successful
+View(so$target_mapping)
+
+# Check the structure of the data with a PCA plot
+# You will get some expected warnings
+
+# Plot Basic PCA:
+# Create custom bold plot elements
+boldTextPlot <- element_text(face = "bold", color = "black", size = 19)
+boldTextAxis <- element_text(face = "bold", color = "black", size = 19)
+
+# Plot Basic PCA:
+# With points
+plot_pca(so, color_by = 'tissue', point_size = 7)
+
+# With labels
+PCA = plot_pca(so, color_by = 'tissue', text_labels = TRUE)
+
+# Assigning the pca plot to the variable 'PCA'
+PCA <- plot_pca(so, color_by = 'tissue', point_size = 7)
+
+# Return the plot to view
+PCA
+
+# Creating plot edits (Bold Elements)
+PCA <- PCA + theme(text = boldTextPlot)
+PCA <- PCA + theme(axis.text = boldTextAxis)
+
+# Repeling the data labels with ggrepel
+PCA <- PCA + 
+  geom_label_repel(aes(label = sample_id, size = 19),
+                   box.padding   = 0.7, 
+                   point.padding = 0.7,
+                   segment.color = 'black', label.size = 0.40)
+
+# Return PCA to check final plot
+PCA
+
+# Create a heatmap for your samples
+plot_sample_heatmap(so, use_filtered = TRUE, color_high = "white", color_low = "dodgerblue",
+                    x_axis_angle = 75)
+
+# Assign the heatmap to the variable 'Heat'
+Heat <- plot_sample_heatmap(so, use_filtered = TRUE, color_high = "white", color_low = "dodgerblue",
+                            x_axis_angle = 75)
+dev.off()
+# Return the plot check that it works
+Heat
+
+# Creating the plot edits (Bold Elements)
+Heat <- Heat + theme(axis.text = boldTextAxis, text = boldTextPlot)
+Heat <- Heat + aes(size = 19, face = "bold")
+
+# Return 'Heat' to check final plot
+Heat
+
+# This Next Section is to Check for Possible outliers
+
+# We can see genes involved in the the 1st PC by looking
+# at the loadings (primary genes whose linear combinations define
+# the principal components)
+
+plot_loadings(so, pc_input = 2)
+
+# Investigate if there are any outliers
+# There may be a possible outliers
+# Investigate further to see if they should be removed
+plot_bootstrap(so, 'ENST00000361789.2', color_by = 'tissue')
+
+
+#*******REMOVAL OF OUTLIERS*****************
+# If there is an outlier, use the folling function to delete it
+
+s2c <- dplyr::filter(s2c, sample != '')
+
+# Set up the new Sleuth Object with the removed outlier.
+# Use this command only if you deleted an outlier
+
+so <- sleuth_prep(s2c,
+                  target_mapping = t2g,
+                  extra_bootstrap_summary = TRUE)
+
+# After the new sleuth object is created, re-analyze the PCA plot to see
+# if the data is distributed any better
+plot_pca(so, color_by = 'condition', point_size = 7)
+plot_pca(so, color_by = 'condition', text_labels = TRUE)
+#*******END REMOVAL OF OUTLIERS**************
+
+
+# Testing for differential genes
+
+# Create the full model (i.e. a model that contains all covariates)
+# then create an additional model with respect to one of the covariates
+# finally compare both models to identify the differences
+
+# In this scenario, the full model will contain just the covariate
+# condition because all the sample under tissue are the same
+so <- sleuth_fit(so, ~tissue, 'full')
+so <- sleuth_fit(so, ~1, 'reduced')
+models(so)
+
+# Create a likelihood ratio test
+so <- sleuth_lrt(so, 'reduced', 'full')
+
+# Create a Wald Test
+so <- sleuth_wt(so, which_beta = 'tissueretina', which_model = 'full')
+
+# Assign results
+retMacSheet <- sleuth_results(so, 'tissueretina', 'wt')
+
+the_results_lrt <- sleuth_results(so, 'reduced:full', 'lrt',
+                                  show_all = FALSE)
+# View Results
+View(retMacSheet) # 188,753 rows of data before cleaning
+retMacSheet_noNA <- na.omit(retMacSheet)
+View(retMacSheet_noNA) #72,794 rows of data after cleaning
+
+
+View(the_results_lrt)
+
+# Filter out the significant genes according to a set qvalue
+sleuth_significant <- dplyr::filter(the_results_wt, qval <= 0.05)
+SigRetinaUpregulated <- dplyr::filter(sleuth_significant, b >= 0)
+SigMaculaUpregulated <- dplyr::filter(sleuth_significant, b <= 0)
+
+# Removal of NA values
+sleuth_significant = na.omit(sleuth_significant)
+
+# View results
+View(sleuth_significant)
+View(SigRetinaUpregulated)
+View(SigMaculaUpregulated)
+
+# Create plot of bootstraps representing differential transcript expression
+plot_bootstrap(obj = so, target_id = 'ENST00000527717.5', 
+               units = 'est_counts')
+
+# View the first 20 genes in this list
+# The number 20 is not a set number and can be changed
+head(sleuth_significant, 20)
+
+# Write the etire gene results table out into a 
+# .csv file. Place the name of file into quotation marks
+write.csv(retMacSheet_noNA,
+          file = "totalData.csv")
+
+# Use Rshiny to do some interactive visualizations
+# Load Rshiny
+sleuth_live(so)
+
+# Creating volcano plot
+
+# Loading necessary packages
+library(sleuth)
+library(ggplot2)
+library(ggrepel)
+
+# Creating volcano variable
+Volcano <- plot_volcano(so, test = "tissueretina", test_type = "wt", which_model = "full",
+                        sig_level = 0.05, point_alpha = 0.2, 
+                        sig_color = "red", highlight = NULL)
+
+Volcano
+
+# Create Cutom Volcano:
+install.packages("dplyr")
+library(dplyr)
+
+# Load in sleuth .csv data
+inputData <- read.csv(file="/Users/ScottSchumacker/Desktop/SciDataPaper/ScientificData/totalData.csv", 
+                 header=TRUE, sep=",")
+
+# mutating the data to tell which points are significant
+inputData <- mutate(inputData, sig=ifelse(inputData$qval<0.05, "FDR < 0.05", "Not Significant"))
+
+View(inputData)
+# search for genes of interest and retrieve their index number
+View(inputData$Gene_Name)
+
+# Assigning gene variable names
+RHO = inputData$Gene_Name[4989]
+PDE6B = inputData$Gene_Name[1143]
+PDE6A = inputData$Gene_Name[4041]
+CNGA1 = inputData$Gene_Name[3667]
+CNGB3 = inputData$Gene_Name[29401]
+GNAT1 = inputData$Gene_Name[2189]
+GNGT1 = inputData$Gene_Name[2211]
+GUCA1A = inputData$Gene_Name[3131]
+GUCA1B = inputData$Gene_Name[1845]
+GUCY2D = inputData$Gene_Name[1824]
+GUCY2F = inputData$Gene_Name[6613]
+RCVRN = inputData$Gene_Name[1216]
+GNB1 = inputData$Gene_Name[1407]
+SAG = inputData$Gene_Name[3522]
+SLC24A1 = inputData$Gene_Name[2123]
+NRL = inputData$Gene_Name[566]
+NR2E3 = inputData$Gene_Name[3270]
+CRX = inputData$Gene_Name[1064]
+
+# Creating the volcano plot variable
+customPlot = ggplot(inputData, aes(b, -log10(qval))) + geom_point(aes(x=b, y=-log10(qval), 
+                                                        color = ifelse(-log10(qval)>-log10(0.05), "Statistically Significant", "no")), size=1.5)
+
+customPlot <- customPlot + geom_point(aes(x=b, y = -log10(qval), color = ifelse(b < -1.5 & -log10(qval) > -log10(0.05) | b > 1.5 & -log10(qval) > -log10(0.05), "Biologically and Statistically Significant" , NA)))
+customPlot <- customPlot + geom_point(aes(x=b, y = -log10(qval), color = ifelse(-log10(qval) < -log10(0.05) & (b) <= -1.5 | (-log10(qval) < -log10(0.05) & (b) > 1.5), "Biologically Significant", NA)))
+customPlot <- customPlot + scale_color_manual(values = c("green", "orange", "black","red"), label = c("no" = "Not Signficant"))
+customPlot
+
+customPlot <- customPlot + geom_vline(xintercept=c(-1.5, 1.5), linetype="dashed")
+customPlot <- customPlot + geom_hline(yintercept=c(1.30), linetype="dashed")
+customPlot
+
+# Labeling the gene names
+customPlot <- customPlot + geom_label_repel(aes(x=b, y = -log10(qval), 
+                              label = ifelse(
+                                
+                                inputData$Gene_Name == RHO, "RHO", ifelse(
+                                  
+                                  inputData$Gene_Name == GUCA1A, "GUCA1A", ifelse(
+                                    
+                                    inputData$Gene_Name == GUCA1B, "GUCA1B", ifelse(
+                                      
+                                      inputData$Gene_Name == GUCY2F, "GUCY2F", ifelse(
+                                                    
+                                                    inputData$Gene_Name == RHO, "RHO", NA)))))), 
+                          color = "black", fontface = "bold", size=3, segment.size = 0.1, box.padding = unit(0.5, "lines"))
+# Plot
+customPlot
